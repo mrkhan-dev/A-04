32bfe36b328b81e3824b2e062c3bbae7ebb3882c
mrkhan-dev <sk5094625@gmail.com>
2024-02-08 08:23:51 +0600

    complete problem no 3

---
+++ problem.js
@@ -0,0 +1,44 @@
+function calculateMoney(ticket) {
+  const ticketPrice = 120;
+  const securityBill = 500;
+  const stuff = 8;
+  const stuffLunchBill = 50 * stuff;
+
+  if (ticket < 0) {
+    return "Only positive number allowed";
+  }
+  const ticketTotal = ticket * ticketPrice;
+  const dailyIncome = ticketTotal - (securityBill + stuffLunchBill);
+  return dailyIncome;
+}
+
+// problem 2
+
+// function checkName(names) {
+//   const characters = ["a", "y", "e", "o", "u", "w"];
+//   const lastLetter = names.length - 1;
+//   if (characters.includes(lastLetter)) {
+//     return "good name";
+//   } else if (typeof names !== "string") {
+//     return "inv";
+//   } else {
+//     return "bad name";
+//   }
+// }
+
+// const checkNames = checkName("ma");
+// console.log(checkNames);
+
+// problem 3
+function deleteInvalids(array) {
+  const filter = [];
+  for (const item of array) {
+    if (typeof item === "number" && !isNaN(item)) {
+      filter.push(item);
+    }
+  }
+  return filter;
+}
+
+const inputArray = deleteInvalids([NaN, 1, 12, 0, -1, undefined, "string"]);
+console.log(inputArray);
